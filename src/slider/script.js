1e51a507f7dbe6c34a23053c8f8d73d229e921a9
arlekin13 <arlekin13rus@gmail.com>
2025-02-09 15:44:31 +0300

    части сайта

---
+++ src/slider/script.js
@@ -0,0 +1,39 @@
+
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 'auto',  
+      spaceBetween: 16,
+      breakpoints: {
+        768: {
+          enabled: false,
+        },
+      },   
+      pagination: {
+        el: '.swiper-pagination',  
+        clickable: true,
+      },
+    });
+
+const showAllButton = document.querySelector('.brands__all');
+const hiddenSlides = document.querySelectorAll('.swiper-slide--hidden');
+const showAllSpan = document.querySelector('.brands__span');
+const showAllImg = document.querySelector('.brands__img');
+let allVisible = false;
+
+showAllButton.addEventListener('click',() => 
+{
+if(allVisible)
+{
+    hiddenSlides.forEach(slide=> slide.classList.add('swiper-slide--hidden'));
+    showAllSpan.textContent = 'Показать все';
+    showAllImg.classList.remove('rotate');
+    allVisible = false;
+}
+else
+{
+    hiddenSlides.forEach(slide => slide.classList.remove('swiper-slide--hidden'));
+    showAllSpan.textContent = 'Скрыть';
+    showAllImg.classList.add('rotate');
+    allVisible = true;
+}
+
+});
